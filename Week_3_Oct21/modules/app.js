d4a980a68c1bb7dfbed4d5e02c3b52126ff658eb
KishorekrD-cypher <kishorekrdeloitte@outlook.com>
2024-10-26 09:15:49 +0000

    Delete Week_3_Oct21 directory

--- Week_3_Oct21/modules/app.js
+++
@@ -1,9 +0,0 @@
-const Logger = require('./logger');
-
-const logger = new Logger();
-
-logger.on('messageLogged',(arg)=>{
-    console.log('Listener called', arg);
-});
-
-logger.log('Message is logger');
